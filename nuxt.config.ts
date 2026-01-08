0869243704c42696374727ff168568017e2e72f2
mamorin <mamosanhaeiyuudesu@yahoo.co.jp>
2026-01-08 13:27:54 +0900

    Vuetifyを導入し、UIコンポーネントを設定。アプリのスタイルを改善。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
 import { defineNuxtConfig } from 'nuxt/config';
+import vuetify from 'vite-plugin-vuetify';
 
 export default defineNuxtConfig({
+  css: ['vuetify/styles'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    plugins: [vuetify({ autoImport: true })],
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
   runtimeConfig: {
     openaiApiKey: '', // 値は入れない
   },
   nitro: {
     preset: 'cloudflare',
   },
-})
+});
